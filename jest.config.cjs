6d47e216076b26b714ab085eba7b05e7945a8a0f
Jami Ylitalo <jami.ylitalo@tuni.fi>
2024-11-27 11:40:56 +0200

    jest working locally

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,7 @@
 module.exports = {
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-};
+    transform: {
+      '^.+\\.js$': 'babel-jest',
+    },
+    transformIgnorePatterns: ['/node_modules/'], // Ensure node_modules is not transformed
+  };
+  
